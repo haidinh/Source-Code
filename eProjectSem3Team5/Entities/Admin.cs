6ad44871dea86cb32db20666c9ba3eba17c93eb6
datpt <datpt.11.1.1992@gmail.com>
2013-08-05 13:57:32 +0700

    commit
    up lên các phần đã làm, xóa 1 số file chưa sử dụng trong entity, chỉnh
    stringconnect trong  web.config

---
+++ eProjectSem3Team5/Entities/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Admin
+    {
+        public int AdminID { set; get; }
+        public string UserName { set; get; }
+        public string PassWord { set; get; }
+        public string Email { set; get; }
+        public string Name { set; get; }
+    }
+}
